18873ec9e61014887e7ad2fa27f41d58d1dbf43b
Lauramars <lauramars85@gmail.com>
2020-12-08 16:43:15 +0100

    AngularFire setup/ Client Service added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAJr1ugZgqnBvOutZH0q2mhY3g0C3dWbQg',
+    authDomain: 'clientpanelcontrol.firebaseapp.com',
+    projectId: 'clientpanelcontrol',
+    storageBucket: 'clientpanelcontrol.appspot.com',
+    messagingSenderId: '373427645439',
+    appId: '1:373427645439:web:1623f880ca69481d13d5ca',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAJr1ugZgqnBvOutZH0q2mhY3g0C3dWbQg',
+    authDomain: 'clientpanelcontrol.firebaseapp.com',
+    projectId: 'clientpanelcontrol',
+    storageBucket: 'clientpanelcontrol.appspot.com',
+    messagingSenderId: '373427645439',
+    appId: '1:373427645439:web:1623f880ca69481d13d5ca',
+  },
 };
 
 /*
