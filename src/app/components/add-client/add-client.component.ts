a7fa302192fe9f1d9829b67d5b35950cdd296310
Lauramars <lauramars85@gmail.com>
2020-12-09 12:51:34 +0100

    Add Client Form & Validation

--- src/app/components/add-client/add-client.component.ts
+++ src/app/components/add-client/add-client.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Client } from '../../models/Client';
+
 @Component({
   selector: 'app-add-client',
   templateUrl: './add-client.component.html',
-  styleUrls: ['./add-client.component.css']
+  styleUrls: ['./add-client.component.css'],
 })
 export class AddClientComponent implements OnInit {
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0,
+  };
 
-  constructor() { }
+  disableBalanceOnAdd: boolean = false;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
