4e84769c5aa3f5c9f8aafd058c325bc5cffb4e52
Lauramars <lauramars85@gmail.com>
2020-12-10 12:35:33 +0100

    Edit & Delete Client

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -43,4 +43,15 @@ export class ClientDetailsComponent implements OnInit {
       timeout: 4000,
     });
   }
+
+  onDeleteClick() {
+    if (confirm('Are you sure?')) {
+      this.clientService.deleteClient(this.client);
+      this.flashMessage.show('Client removed', {
+        cssClass: 'alert-success',
+        timeout: 4000,
+      });
+      this.router.navigate(['/']);
+    }
+  }
 }
